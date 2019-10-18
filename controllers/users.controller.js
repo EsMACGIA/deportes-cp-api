99b1a094e8b0280e6d78179121559b448abb098e
Juan Oropeza <33599807+jaoc1811@users.noreply.github.com>
2019-10-18 09:29:56 -0400

    Created function DELETE/users, which deletes a user from the database (#11)

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -7,6 +7,29 @@ const debug = require('debug')(`${config.debug}controllers:users`)
 const dbPostgres = require('../db/').postgres()
 
 /**
+ * Deletes a user in the database
+ */
+async function deleteUser (id) {
+
+  var data = null
+
+  try {
+
+    data = await dbPostgres.sql('users.deleteUser', { id })
+
+  } catch (error) {
+    // Error handling
+    debug('Error: ', error)
+    data = {
+      error: 'Something is wrong!'
+    }
+  }
+
+  return data
+
+}
+
+/**
  * Gets all user in the database
  */
 async function getAllUsers () {
@@ -54,5 +77,6 @@ async function updateUser (user) {
 
 module.exports = {
   getAllUsers, 
-  updateUser
+  updateUser,
+  deleteUser
 }
--- routes/users/index.js
+++ routes/users/index.js
@@ -6,9 +6,11 @@ const users = require('express').Router()
 // Functions under /
 const getAllUsers = require('./getAllUsers')
 const updateUser = require('./updateUser')
+const deleteUser = require('./deleteUser')
 
 // Endpoints
 users.get('/', getAllUsers)
 users.put('/', updateUser)
+users.delete('/:id', deleteUser)
 
 module.exports = users

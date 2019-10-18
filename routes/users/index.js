354fbef89265aebce379473a61a125f145bc0cef
juan <jaoc1811@gmail.com>
2019-10-18 09:23:25 -0400

    Created function DELETE/users, which deletes a user from the database

--- routes/users/index.js
+++ routes/users/index.js
@@ -5,8 +5,10 @@ const users = require('express').Router()
 
 // Functions under /
 const getAllUsers = require('./getAllUsers')
+const deleteUser = require('./deleteUser')
 
 // Endpoints
 users.get('/', getAllUsers)
+users.delete('/:id', deleteUser)
 
 module.exports = users

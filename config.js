d40241de072dc8c89f3f04754e09235a39f75b5c
Manuel Faria <mfaria724@gmail.com>
2019-10-17 22:18:17 -0400

    PostgreSQL Drivers and dbs configuration. (#4)

--- config.js
+++ config.js
@@ -5,7 +5,16 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const config = {
-  port: process.env.PORT || 3000
+  // Express.js
+  port: process.env.PORT || 3000,
+
+  // PostgreSQL
+  postgres: {
+    user: config.POSTGRES_USER || '',
+    password: config.POSTGRES_PASSWORD || '',
+    port: config.POSTGRES_PORT || '',
+    db: config.POSTGRES_DB || ''
+  }
 }
 
 module.exports = config

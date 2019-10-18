484ab587e79c51e65b08878833abc4e5a71d816a
Manuel Faria <mfaria724@gmail.com>
2019-10-17 21:48:43 -0400

    Express.js Router creation, error handling and 404 routing.

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
 'use strict'
 
-console.log('DeportesCP Backend!')
-console.log('Implementé la funcionalidad de GET /users')
+// Dependencies
+const chalk = require('chalk')
+const debug = require('debug')('deportes-cp-api:index')
+const express = require('express')
+
+// Express.js Configuration
+const app = express()
+
+// Configuration 
+// TODO: Export to a config.js file
+const port = 3000
+
+// TODO: This should be not here, export to a file
+// GET
+app.get('/', (req, res) => {
+  let body = {
+    nameserver: 'DeportesCP Backend',
+    author: 'MACGIA',
+    port: '3000'
+  }
+
+  res.send(body)
+})
+
+// Router for 404 (Not Found)
+app.get('*', (req, res) => {
+  let body = {
+    msg: 'Endpoint Not Found!'
+  }
+
+  res.status(400).send(body)
+})
+
+// Express.js Initializer
+function initExpress() {
+  app.listen(port, () => {
+    console.log(`${chalk.green('[DeportesCP API]:')} Running on port ${chalk.green(port)}`)
+  })
+}
+
+initExpress()
+
+// Error Handling
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
+
+function handleFatalError(err) {
+  debug(`uncaughtException or unhandledRejection: ${err}`)
+} 

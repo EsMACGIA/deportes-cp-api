8720b4e16a316d12fb5db3bb3be79d8c9f3d254a
juan <jaoc1811@gmail.com>
2019-10-22 19:37:40 -0400

    Inform that the ci or email is already in the database

--- routes/users/createUser.js
+++ routes/users/createUser.js
@@ -12,12 +12,8 @@ module.exports = async (req, res) => {
   if (data.constructor === Array) {
     res.status(200)
   } else {
-    data = {
-      error: 'Something is wrong!'
-    }
     res.status(500)
   }
-
   res.send(data)
 
 }

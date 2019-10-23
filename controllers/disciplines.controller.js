bd30b42bd34c59f61e06af3feda52d04481af5b2
Wilfredo Graterol <solo.wilfredo64@gmail.com>
2019-10-22 22:08:46 -0400

    Done #1

--- controllers/disciplines.controller.js
+++ controllers/disciplines.controller.js
@@ -98,8 +98,14 @@ async function updateDiscipline (discipline) {
   } catch (error) {
     // Error handling
     debug('Error: ', error)
-    data = {
-      error: 'Something is wrong!'
+    if (error.queryContext.error.constraint == 'discipline_name_key') {
+      data = {
+        error: 'name_key is already in the database'
+      }
+    }else{
+      data = {
+        error: 'Unidentified error'
+      }
     }
   }
 

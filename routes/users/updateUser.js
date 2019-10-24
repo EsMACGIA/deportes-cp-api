6eb7d135aba91db518b208b365f558994aac278e
Manuel Faria <mfaria724@gmail.com>
2019-10-24 12:25:03 -0400

    Conflict resolution.

--- routes/users/updateUser.js
+++ routes/users/updateUser.js
@@ -7,15 +7,12 @@ module.exports = async (req, res) => {
 
   var data = await usersController.updateUser(req.body)
 
-<<<<<<< HEAD
-=======
   if (data.error) {
     res.status(data.code)
     delete data['code']
   } else {
     res.status(201)
   }
->>>>>>> ebf6bfb76f04afad2053d786256d0f5370f085e3
 
   res.send(data)
 

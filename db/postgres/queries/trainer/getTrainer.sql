7902130f1635ba2d3b4c371a057c0cdec4a87747
JeanPaulYazbek <jeanpaulyf98@hotmail.com>
2019-10-25 00:21:20 -0400

    fixed query to get a trainer

--- db/postgres/queries/trainer/getTrainer.sql
+++ db/postgres/queries/trainer/getTrainer.sql
@@ -1,4 +1,4 @@
--- Get an user information from the database
-SELECT *
-FROM "deportes-cp".users JOIN "deportes-cp".trainer ON user_id = id
+-- Get a trainer information from the database
+SELECT U.id, U.name, lastname, email, ci, D.name as discipline
+FROM ("deportes-cp".trainer JOIN "deportes-cp".users AS U ON user_id=id) JOIN "deportes-cp".discipline AS D ON D.id = discipline_id
 WHERE email = :email
--- db/postgres/queries/trainer/updateTrainer.sql
+++ db/postgres/queries/trainer/updateTrainer.sql
@@ -1,4 +1,4 @@
--- Updates an trainer in the database
+-- Updates a trainer in the database
 UPDATE "deportes-cp".trainer
 SET lastname = :lastname,
     discipline_id = :discipline_id

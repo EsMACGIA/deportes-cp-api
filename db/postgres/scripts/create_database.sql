b4e7af1537be3bc778374e7d65a3e9169267bc0f
Manuel Faria <mfaria724@gmail.com>
2019-10-25 01:30:05 -0400

    Everything fucked it up so i'm trying to save it all.

--- db/postgres/scripts/create_database.sql
+++ db/postgres/scripts/create_database.sql
@@ -7,27 +7,42 @@ CREATE DOMAIN "email_type" AS
   VARCHAR NOT NULL CHECK (value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_\`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
 
 CREATE TABLE users (
+
       id SERIAL,
       email email_type UNIQUE NOT NULL,
       name varchar(30) NOT NULL ,
-      lastname varchar(30) NOT NULL ,
       password varchar(64) NOT NULL,
-      CI integer NOT NULL UNIQUE,
-      type integer NOT NULL,
-
+    
       -- Constraints
       PRIMARY KEY (id),
-      CHECK (0 < type AND type < 4),
-      CHECK (0 < CI AND CI < 999999999),
       CHECK (length(name) > 1),
-      CHECK (length(lastname) > 1),
       CHECK (length(password) > 1)
 );
 
+CREATE TABLE comission (
+
+     user_id integer NOT NULL UNIQUE REFERENCES users
+
+);
+
 CREATE TABLE discipline (
+
      id SERIAL,
      name varchar(128) NOT NULL UNIQUE,
 
      -- Constraints
+     PRIMARY KEY (id),
      CHECK (length(name) > 1)
+);
+
+CREATE TABLE trainer (
+
+     lastname varchar(30) NOT NULL ,
+     user_id integer NOT NULL UNIQUE REFERENCES users,
+     discipline_id integer NOT NULL REFERENCES discipline,
+     CI integer NOT NULL UNIQUE,
+
+     -- Constraints
+     CHECK (length(lastname) > 1),
+     CHECK (0 < CI AND CI < 999999999)
 );

a3553ff0e3eb0c9d2f2d4b844048bf10ca034a04
Wilfredo Graterol <solo.wilfredo64@gmail.com>
2019-11-13 11:55:30 -0400

    deleted a lot od stuff

--- db/postgres/queries/users/getAllUsers.sql
+++ db/postgres/queries/users/getAllUsers.sql
@@ -1,3 +1,4 @@
 -- Get all users in the database
+-- Query not in use
 SELECT *
 FROM "deportes-cp".users
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const routes = require('express').Router()
 
 // Modules
 const root = require('./root')
-const users = require('./users')
 const comissions = require('./comissions')
 const disciplines = require('./disciplines')
 const auth = require('./auth')
@@ -13,7 +12,6 @@ const trainers = require('./trainers')
 
 // Import Modules to Use
 routes.use('/', root)
-routes.use('/users', users)
 routes.use('/comissions', comissions)
 routes.use('/disciplines', disciplines)
 routes.use('/auth', auth)

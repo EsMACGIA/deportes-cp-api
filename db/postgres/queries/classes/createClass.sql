277605840305447f06b065c5ac5884669a0c7823
Manuel Faria <mfaria724@gmail.com>
2019-11-15 09:32:43 -0400

    Trainer id in class.

--- db/postgres/queries/classes/createClass.sql
+++ db/postgres/queries/classes/createClass.sql
@@ -1,3 +1,3 @@
-INSERT INTO "deportes-cp".class (id, description, comission_id)
-VALUES (DEFAULT, :description, :comission_id)
+INSERT INTO "deportes-cp".class (id, description, comission_id, trainer_id)
+VALUES (DEFAULT, :description, :comission_id, :trainer_id)
 RETURNING *

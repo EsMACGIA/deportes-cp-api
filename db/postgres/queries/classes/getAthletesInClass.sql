10cf35244b59b5499d5855c227398f4057d740e1
Wilfredo Graterol <solo.wilfredo64@gmail.com>
2019-12-06 01:10:16 -0400

    query hotfix

--- db/postgres/queries/classes/getAthletesInClass.sql
+++ db/postgres/queries/classes/getAthletesInClass.sql
@@ -1,4 +1,4 @@
-SELECT A.name AS Name, A.lastname AS Lastname, A.ci AS CI, A.stock_number AS Stock_Number, A.sex AS sex, A.birthday AS Birthday
+SELECT A.id AS Id, A.name AS Name, A.lastname AS Lastname, A.ci AS CI, A.stock_number AS Stock_Number, A.sex AS sex, A.birthday AS Birthday
 FROM ("deportes-cp".class AS C 
       JOIN "deportes-cp".athlete_class AS AC ON C.id = AC.class_id) 
       JOIN "deportes-cp".athlete AS A ON A.id = AC.athlete_id
